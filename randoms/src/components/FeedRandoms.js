62d4b9f02b76f0da3a2c2fcd88c0aec19ec79add
LeanM <42581931+LeanM@users.noreply.github.com>
2023-09-24 14:41:28 -0300

    Cambios de google cluster

--- randoms/src/components/FeedRandoms.js
+++ randoms/src/components/FeedRandoms.js
@@ -22,20 +22,12 @@ function useInterval(callback, delay) {
   }, [delay]);
 }
 
-async function getNumero(micro) {
-  //let r = await axios.get("http://" + url + ":" + port);
-  let r = fetch("http://127.0.0.1:32470/" + micro);
-  // fetchRes is the promise to resolve
-  // it by using.then() method
-  r.then((res) => res.json()).then((d) => {
-    console.log(d);
-  });
-
-  return r.data;
-}
+const gateway_endpoint = process.env.REACT_APP_GATEWAY_ENDPOINT;
+const gateway_port = process.env.REACT_APP_GATEWAY_PORT;
 
 function FeedRandoms() {
   const [numero, setNumero] = useState(0);
+  const [clusterIp, setClusterIp] = useState("");
   let indice = Math.floor(Math.random() * requests.length);
   let microservicio = requests[indice];
   let microservice = microservicio.micro;
@@ -43,6 +35,35 @@ function FeedRandoms() {
   const estilo = {
     color: microservicio.color,
   };
+
+  const getClusterIp = async () => {
+    let r = await axios.get("http://" + gateway_endpoint + "/");
+
+    setClusterIp(r.data);
+  };
+
+  useEffect(() => {
+    console.log(gateway_endpoint);
+    console.log(gateway_port);
+  }, []);
+
+  useEffect(() => {
+    console.log(clusterIp);
+  }, [clusterIp]);
+
+  async function getNumero(micro) {
+    let r = await axios.get(
+      "http://" + gateway_endpoint + ":" + gateway_port + "/" + micro
+    );
+    //let r = fetch("http://192.168.49.2:32470/" + micro);
+    // fetchRes is the promise to resolve
+    // it by using.then() method
+    //r.then((res) => res.json()).then((d) => {
+    //  console.log(d);
+    //});
+
+    return r.data;
+  }
 
   console.log(indice);
   useInterval(() => {
